71bd7077648a5e114de8c056df8e950d04da7dff
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-07-01 01:29:16 -0700

    added randomized position for villagers

--- js/Player.js
+++ js/Player.js
@@ -1,6 +1,15 @@
 import Villager from "./Villager.js";
 import Soldier from "./Villager.js";
+import * as GameConstants from "./GameConstants.js";
 
+const TH_LENGHT = GameConstants.TOWNHALL_LENGHT;
+const SPAWN_FACTOR_X = 500;
+const SPAWN_FACTOR_Y = 250;
+
+const P2_TH_POS = {
+  x: GameConstants.P2_TOWNHALL_POSITION_X,
+  y: GameConstants.P2_TOWNHALL_POSITION_Y,
+};
 /*
   This is a Villager class which represents an in-game villager and encapsulates all its functionality
 */
@@ -27,32 +36,45 @@ export default class Player {
         // TODO: Change costs to constants in game. (MAKE CONSTANTS FILE)
         this.movesLeft--;
         this.foodAmount -= 10;
-        this.registry.push(new Villager(this.registry.length));
+        let position = this.findPosition();
+        this.registry.push(new Villager(this.registry.length, position));
       } else {
         alert("Insufficient Food");
       }
     } else {
-      alert('You have no moves remaining please End Turn')
+      alert("You have no moves remaining please End Turn");
     }
   }
 
   createSoldier() {
-      if (this.movesLeft) {
-        if (this.foodAmount >= 10) {
-          if (this.goldAmount >= 10) {
-            this.movesLeft--;
-            this.foodAmount -= 10;
-            this.goldAmount -= 10;
-            this.registry.push(new Soldier(this.registry.length));
-          } else {
-            alert("Insufficient Gold");
-            return;
-          }
+    if (this.movesLeft) {
+      if (this.foodAmount >= 10) {
+        if (this.goldAmount >= 10) {
+          this.movesLeft--;
+          this.foodAmount -= 10;
+          this.goldAmount -= 10;
+          this.registry.push(new Soldier(this.registry.length));
         } else {
-          alert("Insufficient Food");
+          alert("Insufficient Gold");
+          return;
         }
       } else {
-        alert('You have no moves remaining please End Turn')
+        alert("Insufficient Food");
       }
+    } else {
+      alert("You have no moves remaining please End Turn");
+    }
+  }
+
+  findPosition() {
+    let x, y;
+    if (this.color === "blue") {
+      x = Math.floor(Math.random() * SPAWN_FACTOR_X + TH_LENGHT);
+      y = Math.floor(Math.random() * SPAWN_FACTOR_Y + TH_LENGHT);
+    } else {
+      x = P2_TH_POS.x - Math.floor(Math.random() * SPAWN_FACTOR_X + TH_LENGHT);
+      y = P2_TH_POS.y - Math.floor(Math.random() * SPAWN_FACTOR_Y + TH_LENGHT);
+    }
+    return { x, y };
   }
 }
