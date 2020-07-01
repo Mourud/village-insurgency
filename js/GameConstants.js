8bac4d62b31e9eeb4f76a757b072d9727b863f63
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-07-01 00:44:12 -0700

    added game constants file

---
+++ js/GameConstants.js
@@ -0,0 +1,5 @@
+export const PLAYER_ONE_COLOR = "#457b9d";
+export const PLAYER_TWO_COLOR = "#e63946";
+export const INITIAL_POPULATION = 5;
+export const INITIAL_FOOD = 100;
+export const INITIAL_GOLD = 100;
