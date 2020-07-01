a019b02011a8c2b552d312fd9284c6d5d254e482
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-07-01 01:07:35 -0700

    added position to villager

--- js/Villager.js
+++ js/Villager.js
@@ -1,8 +1,9 @@
 /*
   This is a Villager class which represents an in-game villager and encapsulates all its functionality
 */
-export default class Villager{
-  constructor(id) {
+export default class Villager {
+  constructor(id, position) {
     this.id = id;
+    this.position = position;
   }
 }
