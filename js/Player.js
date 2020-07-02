2510556cddb854f42eca0ad70c3f85442cbabd97
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-07-02 00:45:35 -0700

    changed collision detection

--- js/Player.js
+++ js/Player.js
@@ -1,16 +1,18 @@
 import Villager from "./Villager.js";
 import Soldier from "./Soldier.js";
 import * as GameConstants from "./GameConstants.js";
-import {g_MAP} from "./Game.js";
+import { g_MAP } from "./Game.js";
+
+const GAME_WIDTH = GameConstants.GAMEBOX_WIDTH;
+const GAME_HEIGHT = GameConstants.GAMEBOX_HEIGHT;
 
 const TH_LENGHT = GameConstants.TOWNHALL_LENGHT;
-const SPAWN_FACTOR_X = 500;
-const SPAWN_FACTOR_Y = 250;
+const SPAWN_FACTOR_X = 600;
+const SPAWN_FACTOR_Y = 600;
 
 const PLAYER_ONE_COLOR = GameConstants.PLAYER_ONE_COLOR;
 
 const PERSON_COST = GameConstants.PERSON_COST;
-
 
 const P2_TH_POS = {
   x: GameConstants.P2_TOWNHALL_POSITION_X,
@@ -55,7 +57,7 @@ export default class Player {
           this.foodAmount -= PERSON_COST;
           this.goldAmount -= PERSON_COST;
           let position = this.findPosition();
-          this.registry.push(new Soldier(this.registry.length,position));
+          this.registry.push(new Soldier(this.registry.length, position));
         } else {
           alert("Insufficient Gold");
           return;
@@ -69,41 +71,53 @@ export default class Player {
   }
 
   findPosition() {
-    let x,y;
-    if (this.color === PLAYER_ONE_COLOR) {
-      x = Math.floor(Math.random() * SPAWN_FACTOR_X + TH_LENGHT);
-      y = Math.floor(Math.random() * SPAWN_FACTOR_Y + TH_LENGHT);
-    } else {
-      x = P2_TH_POS.x - Math.floor(Math.random() * SPAWN_FACTOR_X + TH_LENGHT);
-      y = P2_TH_POS.y - Math.floor(Math.random() * SPAWN_FACTOR_Y + TH_LENGHT);
-    }
-    let position = { x, y };
+    let position = this.randomPositionGenerator();
     // TODO: IMPLEMENT HASH TABLES?
-    if (this.checkClash(position)) {
-      if (position.x < 1000) {
-        position.x += 10;
-
-      } else {
-        position.x -= 10;
-      }
-      if (position.y < 500) {
-        position.y += 10;
-      } else {
-        position.y += 10;
-      }
-      this.checkClash(position);
+    while (this.checkClash(position)) {
+      position = this.randomPositionGenerator();
     }
-   
+
     g_MAP.push(position);
     return position;
   }
 
+  randomPositionGenerator() {
+    let x, y;
+    if (this.color === PLAYER_ONE_COLOR) {
+      x = Math.floor(Math.random() * SPAWN_FACTOR_X + TH_LENGHT);
+      y = Math.floor(Math.random() * SPAWN_FACTOR_Y);
+    } else {
+      x = P2_TH_POS.x - Math.floor(Math.random() * SPAWN_FACTOR_X + TH_LENGHT);
+      y = P2_TH_POS.y - Math.floor(Math.random() * SPAWN_FACTOR_Y);
+    }
+    let position = { x, y };
+    return position;
+  }
+
   checkClash(position) {
-    g_MAP.forEach(pos => {
-      if (position.x < pos.x + 21 && position.y < pos.y + 21) {
+    for (let i = 0; i < g_MAP.length; i++) {
+      const pos = g_MAP[i];
+      if (this.collidesPosition(position, pos)) {
         return true;
+
       }
-    })
+      
+    }
     return false;
+  }
+
+  collidesPosition(pos1, pos2) {
+    //collision detection in x axis
+    return (
+      this.collidesXY(pos1.x, pos2.x) &&
+      //collision detection in y axis
+      this.collidesXY(pos1.y, pos2.y)
+    );
+  }
+
+  collidesXY(x1, x2) {
+    // console.log((x1 + 40 >= x2 && x1 < x2 + 40))
+
+    return x1 + 40 >= x2 && x1 < x2 + 40;
   }
 }
